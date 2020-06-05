f7523dad28e54cf5c4473855265b3523307cce44
arpapp <trpapp@gmail.com>
2020-06-05 15:35:44 -0600

    db stubbed

---
+++ DB/eventdb.sql
@@ -0,0 +1,48 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema eventdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `eventdb` ;
+
+-- -----------------------------------------------------
+-- Schema eventdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `eventdb` DEFAULT CHARACTER SET utf8 ;
+USE `eventdb` ;
+
+-- -----------------------------------------------------
+-- Table `goal`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `goal` ;
+
+CREATE TABLE IF NOT EXISTS `goal` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS eventuser@localhost;
+SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE USER 'eventuser'@'localhost' IDENTIFIED BY 'eventuser';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'eventuser'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `goal`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eventdb`;
+INSERT INTO `goal` (`id`, `name`) VALUES (1, 'sample');
+
+COMMIT;
+
